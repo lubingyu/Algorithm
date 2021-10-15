c493f9ceaa991959e91749dc3bc48a30cf8fe5b2
lubingyu <2690830350@qq.com>
2021-10-15 19:17:00 +0800

    merge

--- 算法基础课/基础算法/逆序对.cpp
+++ 算法基础课/基础算法/逆序对.cpp
@@ -12,11 +12,23 @@ LL msort(int l, int r) {
 	int mid = l + r >> 1;
 	LL res = msort(l, mid) + msort(mid + 1, r);
 	int k = 0, i = l, j = mid + 1;
-	while (i < mid && j <= r)
+	while (i <= mid && j <= r)
 		if (q[i] <= q[j]) t[k++] = q[i++];
 		else {
 			t[k++] = q[j++];
 			res += mid - i + 1;
 		}
-	while (i <= mid)
+	while (i <= mid) t[k++] = q[i++];
+	while (j <= r) t[k++] = q[j++];
+	for (i = l, j = 0; i <= r; i++, j++) q[i] = t[j];
+	return res;
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) scanf("%d", &q[i]);
+	LL ans = msort(0, n - 1);
+	printf("%lld\n", ans);
+	return 0;
 }
